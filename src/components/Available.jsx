a3d1f63018c9e5c05a4afda170cde71cad27ac88
Asif-vai <mr3366533@gmail.com>
2025-09-23 11:53:46 +0600

    fix responsiveness

--- src/components/Available.jsx
+++ src/components/Available.jsx
@@ -1,25 +1,33 @@
 import React, { use } from 'react'
 import PlayerCards from './PlayerCards';
 
-function AvailablePlayers({promiseData,availableBalance,setAvailableBalance,setPurchesedPlayer,purchesedPlayer}) {
-    const players = use(promiseData);
-    
-  return (
-    <div>
-      
+function AvailablePlayers({
+  promiseData,
+  availableBalance,
+  setAvailableBalance,
+  setPurchesedPlayer,
+  purchesedPlayer
+}) {
+  const players = use(promiseData);
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-5">
-        {
-          players.map(player =><PlayerCards player={player} key={player.id} 
-          availableBalance={availableBalance}
-          setAvailableBalance={setAvailableBalance}
-          purchesedPlayer={purchesedPlayer}
-          setPurchesedPlayer={setPurchesedPlayer}
-          />)
-        }
+  return (
+    <div className="max-w-7xl mx-auto px-4">
+      <h2 className="text-xl font-bold text-gray-800 my-6">Available Players</h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {players.map((player) => (
+          <PlayerCards
+            key={player.id}
+            player={player}
+            availableBalance={availableBalance}
+            setAvailableBalance={setAvailableBalance}
+            purchesedPlayer={purchesedPlayer}
+            setPurchesedPlayer={setPurchesedPlayer}
+          />
+        ))}
       </div>
     </div>
   )
 }
 
-export default AvailablePlayers
+export default AvailablePlayers;
